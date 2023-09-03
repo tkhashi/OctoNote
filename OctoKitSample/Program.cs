587489eaa8f1ae135412eae3034c7c93a4eee8f2
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-09-03 20:48:48 +0900

    first commit

---
+++ OctoKitSample/Program.cs
@@ -0,0 +1,5 @@
+﻿using Octokit;
+
+var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+var user = await github.User.Get("half-ogre");
+Console.WriteLine(user.Followers + " folks love the half ogre!");
